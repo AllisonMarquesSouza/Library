f638502287067530831247e9d10b44b4f80e5215
Allison <allisonsouza10261@gmail.com>
2024-06-06 18:36:50 -0300

    Creating controllers

---
+++ src/main/java/com/br/library/library/controller/UsuarioController.java
@@ -0,0 +1,9 @@
+package com.br.library.library.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+}
